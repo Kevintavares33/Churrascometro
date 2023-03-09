1e47258b9c918d9dac5d32cb8444b7ea69f662d8
@kevintavares33 <kvtavares30@icloud.com>
2023-03-09 16:41:31 -0300

    churrascometro 01
    version 01 project churrascometro.
    
    Co-Authored-By: Kevin Tavares <125915861+kevintavares33@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,53 @@
+
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+let resultado = document.getElementById("resultado");
+
+function calcular() {
+    console.log("calculando");
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos ;
+    let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+    
+
+
+    resultado.innerHTML = `<p>${qdtTotalCarne / 1000} Kg de carne</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalCerveja/355)} latas de cerveja</p>`
+    resultado.innerHTML += `<p>${Math.ceil(qdtTotalBebidas/2000)} Garrafas 2lts de bebidas</p>`
+
+
+
+
+}
+
+
+function carnePP(duracao) {
+    if(duracao>=6) {
+        return 650;
+    }else{
+        return 400;
+    }
+}
+
+function cervejaPP(duracao) {
+    if(duracao>=6) {
+        return 2000;
+    }else{
+        return 1200;
+    }
+}
+
+function bebidasPP(duracao) {
+    if(duracao>=6) {
+        return 1500;
+    }else{
+        return 1000;
+    }
+}
